a58be08635f2a5abfb7e46a846ce71833e365205
kalpanathmajhi <kalpanathmajhi@gmail.com>
2024-09-28 15:59:05 +0530

    added redux toolkit with api calling

---
+++ src/app/store/store.tsx
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../slices/userSlices";
+
+
+// Create the store with proper typing
+export function makeStore() {
+  return configureStore({
+    reducer: {
+      user: userReducer,
+    },
+    devTools: process.env.NODE_ENV !== "production",
+  });
+}
+
+export const store = makeStore();
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
